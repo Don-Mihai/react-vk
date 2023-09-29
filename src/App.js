cd3c3652dfa82edd3de746d119523c39c5bad691
Palienko Mihai <palienko@advantum.pro>
2023-09-29 19:05:25 +0300

    [feat] husky

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import Friends from './modules/Friends';
-import './App.scss';
-import Button from './components/Button';
+import Friends from "./modules/Friends";
+import "./App.scss";
+import Button from "./components/Button";
 
 function App() {
-
-    return (
-        <div>
-            {/* <Friends /> */}
-            <Button name={'Редактировать профиль'} isVisibleIcon={false} ></Button>
-            <Button name={'Еще'} isVisibleIcon={true} ></Button>
-            <Button name={'Увеличить число'} isVisibleIcon={false}></Button>
-        </div>
-    );
+  return (
+    <div>
+      {/* <Friends /> */}
+      <Button name={"Редактировать профиль"} isVisibleIcon={false}></Button>
+      <Button name={"Еще"} isVisibleIcon={true}></Button>
+      <Button name={"Увеличить число"} isVisibleIcon={false}></Button>
+    </div>
+  );
 }
 
 export default App;
