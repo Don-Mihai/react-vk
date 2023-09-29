41e2b73e479476221f04e8b3894dc2e54ad5906e
IraZoloto <9250256135@mail.ru>
2023-09-29 20:32:55 +0300

    pages/register

---
+++ src/Pages/Register/index.js
@@ -0,0 +1,11 @@
+import './Register.scss';
+
+const Register = ({register}) => {
+    return(
+        <div className="register">
+            <h2 className="register__vk">VK for mobile devices</h2>
+        </div>
+    )
+}
+
+export default Register
