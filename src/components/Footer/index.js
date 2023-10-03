b69e65f4ce9332dab5fd63b43edf375c35ca3953
Darya-Shelaeva <140960088+Darya-Shelaeva@users.noreply.github.com>
2023-10-03 20:12:52 +0300

    Revert "footer complete"

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,19 +3,7 @@ import "./footer.scss";
 const Footer = ({ footer }) => {
   return (
     <div className="footer">
-      <a className="footer__vk">О ВКонтакте</a>
-      <p className="years">© 2006-2023</p>
-      <div className="footer__info">
-        <a className="footer__link">О ВКонтакте</a>
-        <a className="footer__link">Правила</a>
-        <a className="footer__link">Для бизнеса</a>
-        <a className="footer__link">Разработчикам</a>
-      </div>
-      <div className="footer__language">
-        <a className="footer__link">Русский</a>
-        <a className="footer__link">Українська</a>
-        <a className="footer__link">все языки »</a>
-      </div>
+      <p>© 2006-2023</p>
     </div>
   );
 };
