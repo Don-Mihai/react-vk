983560bac736469750cb8802fa053467683cf3d6
AnniSona <nastasianasonova@gmail.com>
2023-10-09 07:40:26 -0500

    Друзья

--- src/components/Friend/index.js
+++ src/components/Friend/index.js
@@ -1,13 +1,13 @@
-import './Friend.scss';
+import "./Friend.scss";
+import Avatar from "@mui/material/Avatar";
+const Friend = ({ friend }) => {
+  return (
+    <div className="friend">
+      <Avatar>{friend.name[0]}</Avatar>
+      <h2 className="friend__name">{friend.name}</h2>
+      <h2 className="friend__age">{friend.age}</h2>
+    </div>
+  );
+};
 
-const Friend = ({friend}) => {
-    return(
-        <div className="friend">
-            <div className="friend__image"></div>
-            <h2 className="friend__name">{friend.firstName}</h2>
-            <h2 className="friend__age">{friend.age}</h2>
-        </div>
-    )
-}
-
-export default Friend
+export default Friend;
--- src/modules/Friends/index.js
+++ src/modules/Friends/index.js
@@ -1,57 +1,36 @@
+import axios from "axios";
 import Friend from "../../components/Friend";
-
-const friendsData = [
-    {
-        id: 1,
-        firstName: 'Иван',
-        lastName: 'Иванов',
-        age: 28,
-        location: 'Москва',
-        avatar: 'URL_к_изображению_друга_1',
-    },
-    {
-        id: 2,
-        firstName: 'Анна',
-        lastName: 'Петрова',
-        age: 25,
-        location: 'Санкт-Петербург',
-        avatar: 'URL_к_изображению_друга_2',
-    },
-    {
-        id: 3,
-        firstName: 'Павел',
-        lastName: 'Сидоров',
-        age: 30,
-        location: 'Киев',
-        avatar: 'URL_к_изображению_друга_3',
-    },
-    {
-        id: 4,
-        firstName: 'Екатерина',
-        lastName: 'Козлова',
-        age: 27,
-        location: 'Новосибирск',
-        avatar: 'URL_к_изображению_друга_4',
-    },
-];
+import { useEffect, useState } from "react";
 
 const Friends = () => {
-    return (
-        <div>
-            <h2>Друзья онлайн</h2>
-            <div className="friends">
-                {friendsData.map(friend => {
-                    return <Friend friend={friend} />;
-                })}
-            </div>
-            <h2>Друзья</h2>
-            <div className="friends">
-                {friendsData.map(friend => {
-                    return <Friend friend={friend} />;
-                })}
-            </div>
-        </div>
-    );
+  const [friendsData, setFriendsData] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    const data = await axios.get("http://localhost:3001/users");
+    setFriendsData(data.data);
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <h2>Друзья онлайн</h2>
+      <div className="friends">
+        {friendsData.map((friend) => {
+          return <Friend friend={friend} />;
+        })}
+      </div>
+      <h2>Друзья</h2>
+      <div className="friends">
+        {friendsData.map((friend) => {
+          return <Friend friend={friend} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Friends;
